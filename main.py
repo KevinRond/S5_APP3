635258b6b93ebbc9dc13a69406c4ff24afe97260
Kevin <ronk2602@usherbrooke.ca>
2024-09-29 22:05:14 -0400

    Symphonie done

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from typing import List, Any
 import matplotlib.pyplot as plt
 import numpy as np
 import soundfile as sf
+import wave
+import struct
 
 
 def applyLowpassFilter(signal_data, N):
     lowpassFilter = [1/N] * N
     return np.convolve(lowpassFilter, np.abs(signal_data))
 
-
 def findNearestGainIndex(array, value):
     return (np.abs(array - value)).argmin()
-
 
 def getFilterOrder(w):
     gain = np.power(10, -3/20)
@@ -31,6 +31,68 @@ def getFilterOrder(w):
 
     print("Lowpass Filter Order: " + str(N))
     return N
+
+def generateNotes(lad_freq):
+    la_freq = lad_freq / 1.06
+
+    frequencies = {
+        "do": la_freq * 0.595,
+        "do#": la_freq * 0.630,
+        "re": la_freq * 0.667,
+        "re#": la_freq * 0.707,
+        "mi": la_freq * 0.749,
+        "fa": la_freq * 0.794,
+        "fa#": la_freq * 0.841,
+        "sol": la_freq * 0.891,
+        "sol#": la_freq * 0.944,
+        "la": la_freq,
+        "la#": lad_freq,
+        "si": la_freq * 1.123
+    }
+
+    return frequencies
+
+def synthesizeNoteAudio(fs, harmonics, note_freq, phases, enveloppe, duration=2):
+    ts = np.linspace(0, duration, int(fs * duration))
+    audio = []
+    for t in ts:
+        total = 0
+        for i in range(len(harmonics)):
+            total += harmonics[i] * np.sin(2 * np.pi * note_freq * i * t + phases[i])
+
+        audio.append(total)
+
+    new_env = enveloppe[0:len(audio)]
+    new_env[-int(0.01 * fs):] = np.linspace(new_env[-int(0.01 * fs)], 0, int(0.01 * fs))
+    audio = np.multiply(audio, new_env)
+
+    return audio
+
+def createWav(audio, sampleRate, filename):
+    with wave.open(filename, "w") as wav:
+        nchannels = 1
+        sampwidth = 2
+        nframes   = len(audio)
+        wav.setparams((nchannels, sampwidth, sampleRate, nframes, "NONE", "not compressed"))
+
+        for sample in audio:
+            wav.writeframes(struct.pack('h', int(sample)*10))
+
+def createSilence(sampleRate, duration_s = 1):
+    return [0 for t in np.linspace(0, duration_s , int(sampleRate * duration_s))]
+
+def synthetizeBeethoven(fs, harmonics, phases, enveloppe):
+    solAudio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["sol"], phases, enveloppe, 0.4)
+    miAudio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["re#"], phases, enveloppe, 1.5)
+    faAudio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["fa"], phases, enveloppe, 0.4)
+    reAudio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["re"], phases, enveloppe, 1.5)
+
+    silence = np.zeros(int(fs*0.2))
+
+    fullAudio = np.concatenate((solAudio, silence, solAudio, silence, solAudio, silence, miAudio, silence,
+                                faAudio, silence, faAudio, silence, faAudio, silence, reAudio), axis=None)
+
+    createWav(fullAudio, fs, "5thSymphony.wav")
 
 
 x, fs = sf.read('./note_guitare_lad.wav')
@@ -54,20 +116,21 @@ phases = [np.angle(X[i]) for i in index_harms]
 filterOrder = getFilterOrder(np.pi/1000)
 enveloppeTemporelle = applyLowpassFilter(x, filterOrder)
 
+noteFrequencies = generateNotes(fundamental)
 
-plt.plot(enveloppeTemporelle)
-plt.show()
-
-
-# ts = np.linspace(0, 2, int(fs * 2))
-# audio = []
-# for t in ts:
-#     total = 0
-#     for i in range(len(harmonics)):
-#         total += harmonics[i] * np.sin(2*np.pi*fundamental*i*t + phases[i])
+# LaD_audio = synthesizeNoteAudio(fs, harmonics, fundamental, phases, enveloppeTemporelle)
+# do_audio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["do"], phases, enveloppeTemporelle)
+# re_audio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["re"], phases, enveloppeTemporelle)
+# mi_audio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["mi"], phases, enveloppeTemporelle, 1)
+# fa_audio = synthesizeNoteAudio(fs, harmonics, noteFrequencies["fa"], phases, enveloppeTemporelle)
 #
-#     audio.append(total)
+# createWav(LaD_audio, fs, "laD.wav")
+# createWav(do_audio, fs, "do.wav")
+# createWav(re_audio, fs, "re.wav")
+# createWav(mi_audio, fs, "mi.wav")
+# createWav(fa_audio, fs, "fa.wav")
 
+synthetizeBeethoven(fs, harmonics, phases, enveloppeTemporelle)
 
 
 # FONCTION AFFICHER PLOTS ##########################################
@@ -107,6 +170,3 @@ plt.show()
 # plt.subplot(2, 1, 2)
 # plt.plot(new_x)
 # plt.show()
-#
-# sf.write('test.wav', audio, samplerate=fs)
-
